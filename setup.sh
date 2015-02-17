8fb94cee4c7b32a74ba3479b3c59678345a7cce4
Reed Hedges <reed.hedges@adept.com>
2015-02-17 17:59:37 -0500

    added upstart configs for /etc/ros.launch and flir ptu. fixed name of kinect2_bridge node and pacakge. add ros user to dialout group in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Run as root to add ros group and ros user
 if ! getent group ros; then addgroup ros; fi
-if ! getent passwd ros; then adduser --shell /bin/false --ingroup ros ros --disabled-password; fi
+if ! getent passwd ros; then adduser --shell /bin/false --ingroup ros --add_extra_groups dialout ros --disabled-password; fi
 mkdir -p /var/log/ros
 mkdir -p /var/lib/ros
 chown ros:ros /var/log/ros
